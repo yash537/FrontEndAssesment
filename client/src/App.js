9a1cebf6211ee7fc081e3df9201fc3f5bce1d8f8
yash537 <yashvijaypatel14@gmail.com>
2023-07-25 00:55:41 +0530

    Api Setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,61 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
-	return (
-		<h1>Hello world</h1>
-	);
+  const initialobj = {
+    stockSymbol: "AAPL",
+    date: "2023-07-21",
+  };
+
+  const [formData, setFormData] = useState(initialobj);
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/api/fetchStockData", initialobj)
+      .then((response) => {
+        console.log("API response:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+  }, []);
+
+  const handleFormSave = () => {};
+
+  const onchangeHandler = (e) => {
+    console.log(e);
+    // const newformData = { ...formData, stockSymbol: e.target.value };
+    // setFormData(newformData);
+  };
+  console.log(formData);
+
+  return (
+    <>
+      <form action="submit" onClick={handleFormSave}>
+        <div>
+          <label>Symbol of Stock :</label>
+          <input
+            type="text"
+            placeholder="Enter symbol of stock"
+            onChange={(e) => {
+              onchangeHandler();
+            }}
+          ></input>
+        </div>
+        <div>
+          <label>Date :</label>
+          <input
+            type="date"
+            placeholder="select a date"
+            onChange={(event) => {
+              onchangeHandler();
+            }}
+          ></input>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,34 +1,59 @@
 // DO NOT MODIFY ANYTHING HERE, THE PLACE WHERE YOU NEED TO WRITE CODE IS MARKED CLEARLY BELOW
 
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 
 const app = express();
 
 app.use(function (req, res, next) {
-    const allowedOrigins = ['http://localhost:3000'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
+  const allowedOrigins = ["http://localhost:3000"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
 });
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
-app.post('/api/fetchStockData', (req, res) => {
-    // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
-    res.sendStatus(200);
+app.post("/api/fetchStockData", (req, res) => {
+  // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
+  // https://api.polygon.io/v1/open-close/AAPL/2023-07-21
+  // https://api.polygon.io/v1/open-close/{stocksTicker}/{date}
+
+  const { stockSymbol, date } = req.body;
+  console.log(stockSymbol, date);
+  const token = "MLbrHkKAClA7yA3I54yCNxwwTvK7GiPg";
+
+  axios
+    .get(`https://api.polygon.io/v1/open-close/${stockSymbol}/${date}`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+    .then((response) => {
+      console.log("API response:", response.data);
+      return res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
+    });
+  console.log("hello from api side");
 });
 
 const port = process.env.PORT || 5000;
